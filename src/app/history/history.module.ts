85361223617dc07a8014bd1f7aaa13a76f382b19
omkarsunildhapte <67897741+omkarsunildhapte@users.noreply.github.com>
2023-11-09 19:11:14 +0530

    add the project

---
+++ src/app/history/history.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentFormComponent } from './student-form/student-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    StudentFormComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    StudentFormComponent
+  ]
+})
+export class HistoryModule { }
